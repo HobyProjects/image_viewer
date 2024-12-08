90923e115ee024d523c464d0d73dc8b3ae9667bd
HobyProjects <hobbyprojects.dev@gmail.com>
2024-12-08 12:30:20 +0530

    Add Timer class implementation and update CMakeLists to include timer support

---
+++ src/core/subcore/timer.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "typedef.hpp"
+
+namespace IMGV::Core
+{
+    class IMGV_API Timer
+    {
+        public:
+            Timer() = default;
+            Timer(Float deltaTime) : m_DeltaTime(deltaTime) {}
+            ~Timer() = default;
+
+            Float GetDeltaTime() const { return m_DeltaTime; }
+            Float GetDeltaTimeSeconds() const { return m_DeltaTime; }
+            Float GetDeltaTimeInMilliseconds() const { return m_DeltaTime * 1000.0f; }
+            operator Float() const { return m_DeltaTime; }
+
+        private:
+            Float m_DeltaTime{0.0f};
+    };
+}
