8ad9795b2374010986585d35b5d7af86666d5355
HobyProjects <hobbyprojects.dev@gmail.com>
2024-12-11 20:57:07 +0530

    Add application entry point and update CMakeLists for img_view executable

---
+++ src/core/core.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "typedef.hpp"
+#include "instrument.hpp"
+#include "keycodes.hpp"
+#include "layer.hpp"
+#include "layer_stack.hpp"
+#include "window.hpp"
+#include "timer.hpp"
+#include "log.hpp"
+
+#include "renderer.hpp"
+#include "buffers.hpp"
+#include "shader.hpp"
+#include "texture.hpp"
+#include "camera.hpp"
+#include "camera_2d.hpp"
+#include "vertex_array.hpp"
+#include "frame_buffer.hpp"
+#include "context.hpp"
+
+#include "events.hpp"
+#include "inputs_handler.hpp"
+#include "events_receiver.hpp"
+#include "window_events.hpp"
+#include "mouse_events.hpp"
+#include "keyboard_events.hpp"
